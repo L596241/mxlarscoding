1a3710dc7a125acbfc602c4baa76a4c8fbc65ae8
mxlars <89201443+L596241@users.noreply.github.com>
2023-03-04 18:59:25 +0000

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,17 +129,21 @@
    * Hero type effect
    */
   const typed = select('.typed')
-  if (typed) {
-    let typed_strings = typed.getAttribute('data-typed-items')
-    typed_strings = typed_strings.split(',')
-    new Typed('.typed', {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
-    });
-  }
+if (typed) {
+  let typed_strings = typed.getAttribute('data-typed-items')
+  typed_strings = typed_strings.split(',')
+  new Typed('.typed', {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000,
+    smartBackspace: true,
+    shuffle: true
+  });
+}
+
+
 
   /**
    * Skills animation
