8fa416e24a021a17032551805f1ef270ee2ba7ae
Lars Erik Risholm <89201443+L596241@users.noreply.github.com>
2024-01-01 12:41:21 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,26 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+
+  /**
+   * Automatically increment my Birthday
+   */
+function calculateAge(birthDateString) {
+    var birthDate = new Date(birthDateString);
+    var today = new Date();
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+  }
+
+  document.getElementById('age').textContent = calculateAge('1989-12-07');
+
+
+
+
 (function() {
   "use strict";
 
